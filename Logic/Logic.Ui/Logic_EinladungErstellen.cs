1f15ceabd1fe1f9d496c842eec4e14ddceaef6f1
Albers, Torben <torben.albers@tsbw.cloud>
2022-02-09 13:59:32 +0100

    Bindings Buttons

---
+++ Logic/Logic.Ui/Logic_EinladungErstellen.cs
@@ -0,0 +1,55 @@
+﻿using GalaSoft.MvvmLight.CommandWpf;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace JaegerMeister.MvvmSample.Logic.Ui
+{
+    public class Logic_EinladungErstellen : MainViewModel, INotifyPropertyChanged
+    {
+        private ICommand _btn_einlandungSenden;
+        public ICommand btn_EinlandungSenden
+        {
+            get
+            {
+                if (_btn_einlandungSenden == null)
+                {
+                    _btn_einlandungSenden = new RelayCommand(() =>
+                    {
+                        
+
+                        ///Logic
+                    });
+                }
+                return _btn_einlandungSenden;
+            }
+        }
+
+       
+        private ICommand _btn_abbrechen;
+        public ICommand btn_Abbrechen
+        {
+            get
+            {
+                if (_btn_abbrechen == null)
+                {
+                    _btn_abbrechen = new RelayCommand(() =>
+                    {
+                       
+
+                        ///Logic
+                    });
+                }
+                return _btn_abbrechen;
+            }
+        }
+
+        private ObservableCollection<Logic_EinladungErstellen> _dg_Termine;
+        public
+    }
+}
--- Logic/Logic.Ui/ViewModelLocator.cs
+++ Logic/Logic.Ui/ViewModelLocator.cs
@@ -31,10 +31,11 @@ namespace JaegerMeister.MvvmSample.Logic.Ui
             }
 
             SimpleIoc.Default.Register<MainViewModel>();
+            SimpleIoc.Default.Register<Logic_EinladungErstellen>();
         }
 
         public MainViewModel Main => ServiceLocator.Current.GetInstance<MainViewModel>();
-
+        public MainViewModel logic_einladungErstellen => ServiceLocator.Current.GetInstance<MainViewModel>();
         public static void Cleanup()
         {
             
