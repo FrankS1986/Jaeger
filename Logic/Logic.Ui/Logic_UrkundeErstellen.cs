9b65448a00cbd965f4548b5f98a225186013c179
Julia Kreikenbaum <julia.kreikenbaum@tsbw.cloud>
2022-02-09 13:57:01 +0100

    Properties

--- Logic/Logic.Ui/Logic_UrkundeErstellen.cs
+++ Logic/Logic.Ui/Logic_UrkundeErstellen.cs
@@ -8,6 +8,20 @@ namespace JaegerMeister.MvvmSample.Logic.Ui
 {
     public class Logic_UrkundeErstellen : ViewModelBase, INotifyPropertyChanged
     {
-
+        private ICommand _btn_Urkunden_erstellen;
+        public ICommand btn_Urkunden_erstellen
+        {
+            get 
+            {
+                 if (_btn_Urkunden_erstellen==null)
+                 {
+                    _btn_Urkunden_erstellen = new RelayCommand(() =>
+                    {
+                        Logic_UrkundeErstellen logic = new Logic_UrkundeErstellen();
+                    });
+                 }
+                return _btn_Urkunden_erstellen;
+            }
+        }
     }
 }
