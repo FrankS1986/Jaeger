0793359461153b53616e022478d4a9ebdd142809
Frank Schroeder <frank.schroeder@tsbw.cloud>
2022-02-14 15:41:18 +0100

    anmeldefenster neu erstellt unter feature/neues_login

---
+++ Ui/Ui.Desktop/GUILogin.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace JaegerMeister.MvvmSample.Ui.Desktop
+{
+    /// <summary>
+    /// Interaktionslogik für GUILogin.xaml
+    /// </summary>
+    public partial class GUILogin : Window
+    {
+        public GUILogin()
+        {
+            InitializeComponent();
+        }
+
+        private void PwtNeu_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
