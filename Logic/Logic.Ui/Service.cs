4841b476ac39f6519d669bdb937db63e561356c6
Julia Kreikenbaum <julia.kreikenbaum@tsbw.cloud>
2022-02-17 11:43:07 +0100

    Nachträgliche verbesserungen

--- Logic/Logic.Ui/Service.cs
+++ Logic/Logic.Ui/Service.cs
@@ -9,44 +9,27 @@ namespace JaegerMeister.MvvmSample.Logic.Ui
         public bool loginSuccessful;
         public bool LoginSuccessful(string benutzer, string passwort)
         {
-           
+
             using (TreibjagdTestEntities ctx = new TreibjagdTestEntities())
             {
-                Logic_Login Logik = new Logic_Login();
-
-
-
-                var loginData = from a in ctx.tbl_Login
+               var loginData = from a in ctx.tbl_Login
                                 where a.Loginname == benutzer && a.Passwort == passwort
                                 select new { a.Loginname, a.Passwort };
 
-
-
-                var LoginData = new List<tbl_Login>();
-                foreach (var item in loginData)
+                if (loginData.Count() == 1)
                 {
-                    LoginData.Add(new tbl_Login()
-                    {
-
-                        Loginname = item.Loginname,
-                        Passwort = item.Passwort
-                    });
-
-                    if (item.Loginname == null && item.Passwort == null )
-                    {
-                        loginSuccessful = false;
-                    }
-                    else
-                    {
-                        loginSuccessful = true;
-                    }
+                    loginSuccessful = true;
+                }
+                else
+                {
+                    loginSuccessful = false;
                 }
                 return loginSuccessful;
             }
 
         }
 
-        
+
 
     }
 }
